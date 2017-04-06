06b83791f35d90712686a51af5f1b012a2a88ac1
Leonardo Flores <leonardocouy@hotmail.com>
2017-04-05 21:29:46 -0300

    Mudanças severas na estrutura

---
+++ chatbot-util.js
@@ -0,0 +1,79 @@
+function ChatBotUtil() {
+  var self = this;
+
+  return self;
+}
+
+ChatBotUtil.prototype.createOption = function(order, caption, value){
+  var content;
+
+  if (typeof(value) !== 'undefined'){
+    content = {
+      "type": "application/json",
+      "order": order,
+      "text": caption,
+      "value": value
+    };
+  }else{
+    content = {
+      "type": "application/json",
+      "order": order,
+      "text": caption
+    };
+  }
+  return content;
+}
+
+
+ChatBotUtil.prototype.buildSelectMenu = function(title, options){
+  return {
+    "type": "application/vnd.lime.select+json",
+    "content": {
+      "text": title,
+      "options": options
+    }
+  };
+}
+
+ChatBotUtil.prototype.createMediaHeader = function(title, text, image_uri){
+  return {
+    "type": "application/vnd.lime.media-link+json",
+    "value": {
+      "title": title,
+      "text": text,
+      "type": "image/jpeg",
+      "uri": image_uri
+    }
+
+  };
+}
+
+ChatBotUtil.prototype.createMediaItem = function(label, value){
+  return [{
+    "label": {
+      "type": "text/plain",
+      "value": label
+    },
+    "value": {
+      "type": "application/json",
+      "value": value
+    }
+  }];
+}
+
+ChatBotUtil.prototype.buildMediaMenu = function(items){
+  return {
+    "itemType": "application/vnd.lime.document-select+json",
+    "items": items
+  };
+
+}
+ChatBotUtil.prototype.buildCollection = function(menu){
+  return {
+    "type": "application/vnd.lime.collection+json",
+    "content": menu
+  };
+}
+
+
+module.exports = ChatBotUtil;
